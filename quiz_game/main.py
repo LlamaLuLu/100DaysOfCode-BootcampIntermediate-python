6195d89eaa834bbdfb28b9d0393bfd9023458df7
Lulama Lingela <lulamahana@gmail.com>
2023-06-19 00:15:41 +0200

    Add files via upload

---
+++ quiz_game/main.py
@@ -0,0 +1,14 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+# converting str -> obj
+question_bank = []
+for q in question_data:
+    question_bank.append(Question(q["text"], q["answer"]))  # adds new question
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+
