a5246b730e708d3b5e5ad431633a41bfa2057de7
Lulama Lingela <lulamahana@gmail.com>
2023-06-20 22:26:19 +0200

    Add files via upload

---
+++ mail_merge_proj_day24/main.py
@@ -0,0 +1,13 @@
+# TODO: Create a letter using starting_letter.txt
+
+with open("./Input/Names/invited_names.txt") as name_list:
+    names = name_list.read().splitlines()
+
+with open("./Input/Letters/starting_letter.txt") as template:
+    letter_template = template.read()
+
+for name in names:
+    with open(f"./Output/ReadyToSend/Letter to {name}.txt", "w") as letter:
+        final_letter = letter_template.replace("[name]", name)
+        final_letter = final_letter.replace("Angela", "Lulama")
+        letter.write(final_letter)
